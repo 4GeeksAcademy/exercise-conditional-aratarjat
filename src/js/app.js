c32deab334d39ec102d422df1541f834911e029f
Anderson Aguilera <124855089+arat-arjat@users.noreply.github.com>
2024-05-11 02:20:26 +0000

    culminado

--- src/js/app.js
+++ src/js/app.js
@@ -31,15 +31,29 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+            ${variables.includeCover && cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+          <h1>${variables.name || "Your Name"} ${variables.lastName ||
+    "Your Lastname"}</h1>
+          <h2> ${variables.role || "Web Developer "} </h2>
+          <h3>${variables.city || "City"}, ${variables.country ||
+    "Country"}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${
+              variables.twitter
+                ? "https://twitter.com/" + variables.twitter
+                : "https://twitter.com/4geeksacademy"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github
+                ? "https://github.com/" + variables.github
+                : "https://github.com/4geeksacademy"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin
+                ? "https://www.linkedin.com/" + variables.linkedin
+                : "https://linkedin.com/school/4geeksacademy"
+            }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
